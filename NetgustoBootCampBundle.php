8c24e8a21acedd5847e15e1810de094588a721e6
Net Gusto / Jérôme Schneider <contact@netgusto.com>
2014-07-22 13:30:27 +0200

    WIP

--- NetgustoBootCampBundle.php
+++ NetgustoBootCampBundle.php
@@ -4,11 +4,13 @@ namespace Netgusto\BootCampBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Netgusto\DevServerBundle\NetgustoDevServerBundle;
+use Netgusto\AutorouteBundle\NetgustoAutorouteBundle;
 
 class NetgustoBootCampBundle extends Bundle
 {
     public function __construct(array &$bundles) {
         $bundles[] = $this;
         $bundles[] = new NetgustoDevServerBundle();
+        $bundles[] = new NetgustoAutorouteBundle();
     }
 }
